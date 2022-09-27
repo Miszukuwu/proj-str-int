b829759e33317bb709df92a687aec92253d505f5
Duzy-Dym <56600554+Duzy-Dym@users.noreply.github.com>
2022-09-27 10:07:49 +0200

    Update Pierwszy.cs

--- Progr.Apl/Pierwszy.cs
+++ Progr.Apl/Pierwszy.cs
@@ -1,10 +1,17 @@
-﻿using System;
+using System;
 
     class program
     {
         
         static void Main()
         {
-            Console.WriteLine("Hello World");
+            Console.Write("Podaj długość boku a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Podaj długość wysokość h: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            int p = ((a*h)/2);
+            Console.WriteLine("Pole trójkąta wynosi " + p);
         }
     }
