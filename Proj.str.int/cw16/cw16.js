5ff292c94ecff27e2000cab9a5cccebe1f1e9c68
Duzy-Dym <56600554+Duzy-Dym@users.noreply.github.com>
2022-11-21 17:45:58 +0100

    Add files via upload

---
+++ Proj.str.int/cw16/cw16.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll("input[type=range]");
+console.log(inputs);
+for(let i = 0;i<inputs.length; i++)
+{
+    inputs[i].nextElementSibling.innerHTML = inputs[i].value;
+    inputs[i].addEventListener("input",function(e){
+        console.log(e.target.value);
+        e.target.nextElementSibling.innerHTML = e.target.value;
+    });
+}
