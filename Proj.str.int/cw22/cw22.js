6fe224f22b65446556bbe6c687b29dd1c52c0d62
Duzy-Dym <aleksanderowsinski@interia.pl>
2023-03-06 17:51:44 +0100

    AAAAAAAAAAAAAAAAAAA

---
+++ Proj.str.int/cw22/cw22.js
@@ -0,0 +1,22 @@
+const books = [
+    "ABC programowania w JS",
+    "Rybki akwariowe",
+    "Góry polskie - przewodnik",
+    "Encyklopedia głupoty"
+]
+const edit = "<img src='media/edit.png' id='edit'>";
+const del = "<img src='media/delete.png' id='del'>";
+function generBooks(books)
+    {
+        let html  = "<ul>";
+        for(let b of books){
+            html += `<li><span class="text">${b}</span> ${edit} ${del}</li>`;
+        }
+        return html+"</ul>";
+    }
+document.querySelector("#addNew").onclick = function()
+    {
+        books.push(document.querySelector("#newTitle").value);
+        document.querySelector("#list").innerHTML = generBooks(books);
+    }
+document.querySelector("#list").innerHTML = generBooks(books);
